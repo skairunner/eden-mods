6f3ed2bcfb3e1603590566273b9b474553ff45fa
Skye Im <fluffyterran@gmail.com>
2020-04-15 19:17:28 +0200

    Initial hello world

---
+++ DrywallHidesPipe/ClarityPatch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using HarmonyLib;
+
+namespace Clarity
+{
+    public static class ClarityPatch
+    {
+        [HarmonyPatch(typeof(Player), "Start")]
+        public static class Player_Start
+        {
+            public static void Postfix()
+            {
+                Log.Initialize();
+                Log.WriteLine("Hello World");
+            }
+        }
+    }
+}
---
+++ DrywallHidesPipe/Log.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace Clarity
+{
+    public static class Log
+    {
+        private static StreamWriter log;
+
+        private static string _logFileLocation;
+        public static string LogFileLocation
+        {
+            get => _logFileLocation;
+            set
+            {
+                _logFileLocation = value;
+                log = File.CreateText(value);
+            }
+        }
+
+        public static void Initialize()
+        {
+            if (log == null)
+            {
+                var ass = System.Reflection.Assembly.GetEntryAssembly();
+                var location = ass == null ? @"C:\coding\eden-mods\DrywallHidesPipe" : ass.Location;
+                LogFileLocation = Path.Combine(location, "output.log");
+            }
+        }
+
+        public static void WriteLine(string line)
+        {
+            log.WriteLine(line);
+            log.Flush();
+        }
+    }
+}
