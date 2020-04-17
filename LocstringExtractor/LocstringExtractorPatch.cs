ff8ba771f3d1623ccefdf7b5430f7c0c869d0afa
Skye Im <skairunner@gmail.com>
2020-04-17 20:14:16 +0200

    Locstring Extractor initial

---
+++ LocstringExtractor/LocstringExtractorPatch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using HarmonyLib;
+using I2.Loc;
+using UnityEngine;
+
+namespace LocstringExtractor
+{
+    public static class LocstringExtractorPatch
+    {
+        [HarmonyPatch(typeof(Player), "Start")]
+        public static class Player_Start
+        {
+            public static void Postfix()
+            {
+                var userdir = Environment.GetFolderPath(System.Environment.SpecialFolder.UserProfile);
+                var output = File.OpenWrite(Path.Combine(userdir, "loc.txt"));
+                var writer = new StreamWriter(output);
+                // just ransack the entire localization file
+                foreach (var term in LocalizationManager.GetTermsList())
+                {
+                    var translation = LocalizationManager.GetTermData(term).GetTranslation(0);
+                    writer.WriteLine($"{term}\t{translation}");
+                }
+
+                output.Flush();
+                output.Close();
+            }
+        }
+    }
+}
